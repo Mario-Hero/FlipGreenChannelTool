5b6850ced7f2e92578c7450120e5fc698d06aaa5
Mario Hero <mariocanfly@126.com>
2023-05-17 19:45:47 +0800

    Add files via upload

---
+++ FlipGreenChannelTool.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <fstream>
+
+using namespace cv;
+const std::string flipStr = "-GFLIP";
+
+bool isFileExists(std::string& name) {
+    std::ifstream f(name.c_str());
+    return f.good();
+}
+
+int handle(char* ch) {
+    const std::string imagePath = ch;
+    std::string suffix = imagePath;
+    std::string newImagePrefix = imagePath;
+    std::string newImagePath;
+
+    std::cout << "Input: " << imagePath << std::endl;
+
+    for (int i = imagePath.size() - 1; i > 0; --i)
+    {
+        if (imagePath[i] == '.')
+        {
+            newImagePrefix = imagePath.substr(0, i);
+            suffix = imagePath.substr(i + 1);
+            break;
+        }
+    }
+    if (suffix == imagePath) {
+        std::cout << "Cannot find a dot in " << imagePath << std::endl;
+        suffix = "";
+    }
+    newImagePath = suffix == "" ? newImagePrefix + flipStr : newImagePrefix + flipStr + "." + suffix;
+    if (isFileExists(newImagePath)) {
+        std::cout << newImagePath << " already exists." << std::endl;
+        system("pause");
+        return 3;
+    }
+    bool containsflipStr = true;
+    for (size_t i = 0; i < flipStr.size(); ++i)
+    {
+        if (newImagePrefix[newImagePrefix.size() - 1 - i] != flipStr[flipStr.size() - 1 - i]) {
+            containsflipStr = false;
+            break;
+        }
+    }
+    if (containsflipStr) {
+        newImagePath = suffix == "" ? newImagePrefix.substr(0, newImagePrefix.size() - flipStr.size()) : newImagePrefix.substr(0, newImagePrefix.size() - flipStr.size()) + "." + suffix;
+        if (isFileExists(newImagePath)) {
+            std::cout << newImagePath << " already exists." << std::endl;
+            system("pause");
+            return 3;
+        }
+    }
+
+    transform(suffix.begin(), suffix.end(), suffix.begin(), tolower);
+    Mat image = imread(imagePath, IMREAD_UNCHANGED);
+    if (image.empty()) {
+        std::cout << "Cannot read " << imagePath << std::endl;
+        system("pause");
+        return 1;
+    }
+    //std::cout << image.size << endl;
+    //std::cout << image.depth() << endl;
+    //std::cout << image.channels() << endl;
+    if (image.channels() < 3) {
+        std::cout << imagePath << " has less than 3 channels." << std::endl;
+        system("pause");
+        return 2;
+    }
+    else if (image.channels() == 3) {
+        switch (image.depth()) {
+        case CV_16F:
+        case CV_32F: {
+            auto it_beg = image.begin<Vec3f>();
+            const auto it_end = image.end<Vec3f>();
+            for (; it_beg != it_end; ++it_beg) {
+                (*it_beg)[1] = 1.0 - (*it_beg)[1];
+            }
+            break;
+        }
+        case CV_8U: {
+            auto it_beg = image.begin<Vec3b>();
+            const auto it_end = image.end<Vec3b>();
+            for (; it_beg != it_end; ++it_beg) {
+                (*it_beg)[1] = 255 - (*it_beg)[1];
+            }
+            break;
+        }
+        default: {
+            std::cout << imagePath << " has unknown image type." << std::endl;
+            system("pause");
+            break;
+        }
+        }
+    }
+    else if (image.channels() == 4) {
+        switch (image.depth()) {
+        case CV_16F:
+        case CV_32F: {
+            auto it_beg = image.begin<Vec4f>();
+            const auto it_end = image.end<Vec4f>();
+            for (; it_beg != it_end; ++it_beg) {
+                (*it_beg)[1] = 1.0 - (*it_beg)[1];
+            }
+            break;
+        }
+        case CV_8U: {
+            auto it_beg = image.begin<Vec4b>();
+            const auto it_end = image.end<Vec4b>();
+            for (; it_beg != it_end; ++it_beg) {
+                (*it_beg)[1] = 255 - (*it_beg)[1];
+            }
+            break;
+        }
+        default: {
+            std::cout << imagePath << " has unknown image type." << std::endl;
+            system("pause");
+            break;
+        }
+        }
+    }
+    else {
+        std::cout << imagePath << " has morn than 4 channels." << std::endl;
+        system("pause");
+        return 3;
+    }
+    std::vector<int> param;
+    switch (image.depth()) {
+    case CV_16F: {
+        param = { IMWRITE_EXR_TYPE , IMWRITE_EXR_TYPE_HALF };
+        break;
+    }
+    case CV_32F: {
+        param = { IMWRITE_EXR_TYPE , IMWRITE_EXR_TYPE_FLOAT };
+        break;
+    }
+    case CV_8U: {
+        if (suffix == "png") {
+            param = { IMWRITE_PNG_COMPRESSION , 1 };
+        }
+        else if (suffix == "jpg" || suffix == "jpeg") {
+            param = { IMWRITE_JPEG_QUALITY , 95 };
+        }
+        break;
+    }
+    }
+    /*
+    int i = 1;
+    while (isFileExists(newImagePath)) {
+        newImagePath = newImagePrefix + flipStr + std::to_string(i) + "." + suffix;
+        ++i;
+    }
+    */
+    imwrite(newImagePath, image, param);
+    std::cout << "Complete." << std::endl;
+    return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+    if (argc <= 1) {
+        std::cout << "Please input image files." << std::endl;
+        return 0;
+    }
+    _putenv("OPENCV_IO_ENABLE_OPENEXR=1");
+    for (size_t i = 1; i < argc; ++i) {
+        handle(argv[i]);
+    }
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# FlipGreenChannelTool 
+
+能把图片的绿色通道反转的工具，用于OpenGL和DirectX法线贴图的相互转换。支持JPG/PNG/TIFF/EXR图片格式。
+
+A tool which can invert the green channel of pictures in order to convert normal maps between format OpenGL and DirectX. Support JPG/PNG/TIFF/EXR.
+
+## 如何使用？ How to use it?
+
+把图片拖到FlipGreenChannelToolPack.exe上即可转换，另存为新的文件。
+
+Drag the image to 'FlipGreenChannelToolPack.exe', then it will invert its green channel and save as another file.
+
+## 为何使用？Why use it?
+
+OpenGL和DirectX法线贴图只有绿色通道是相反的，可以互相转换。在Unreal Engine中是可以在导入设置中设置“反转绿色通道”来完成转换的，很方便，不需要使用该程序，但是其他情况下可能需要这样一个软件了。
+
+The only difference between OpenGL and DirectX Normal maps is the opposite green channel, and they can be converted to each other. In Unreal Engine, it is possible to set the "Invert Green Channel" in the import settings to complete the conversion, which is convenient and does not require the this program. However, in other cases, such a program may be useful.
+
+## 下载 Download
+
+
+
+## 依赖 Dependency
+
+Windows x64
+
+OpenCV 4.7 (Maybe OpenCV 4+ is ok)
+
+## 编译 Build
+
+本程序需要OpenCV开启EXR支持，所以需要手动编译OpenCV并添加OpenEXR支持。
+
+This program requires EXR format support, so you need to manually compile OpenCV and add OpenEXR support.
+
+## License
+
+This project is licensed under the Apache License, Version 2.0
