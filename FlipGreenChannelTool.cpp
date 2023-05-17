b862f61598062f0dd2a5314d9aae1681be2e6b61
Mario Hero <mariocanfly@126.com>
2023-05-17 20:03:02 +0800

    Update FlipGreenChannelTool.cpp

--- FlipGreenChannelTool.cpp
+++ FlipGreenChannelTool.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-#include <string>
-#include <opencv2/opencv.hpp>
 #include <fstream>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
 
 using namespace cv;
 const std::string flipStr = "-GFLIP";
 
-bool isFileExists(std::string& name) {
+bool isFileExists(const std::string &name) {
     std::ifstream f(name.c_str());
     return f.good();
 }
 
-int handle(char* ch) {
+int handle(const char *ch) {
     const std::string imagePath = ch;
     std::string suffix = imagePath;
     std::string newImagePrefix = imagePath;
@@ -19,10 +19,8 @@ int handle(char* ch) {
 
     std::cout << "Input: " << imagePath << std::endl;
 
-    for (int i = imagePath.size() - 1; i > 0; --i)
-    {
-        if (imagePath[i] == '.')
-        {
+    for (int i = imagePath.size() - 1; i > 0; --i) {
+        if (imagePath[i] == '.') {
             newImagePrefix = imagePath.substr(0, i);
             suffix = imagePath.substr(i + 1);
             break;
@@ -32,22 +30,28 @@ int handle(char* ch) {
         std::cout << "Cannot find a dot in " << imagePath << std::endl;
         suffix = "";
     }
-    newImagePath = suffix == "" ? newImagePrefix + flipStr : newImagePrefix + flipStr + "." + suffix;
+    newImagePath = suffix == "" ? newImagePrefix + flipStr
+                                : newImagePrefix + flipStr + "." + suffix;
     if (isFileExists(newImagePath)) {
         std::cout << newImagePath << " already exists." << std::endl;
         system("pause");
         return 3;
     }
     bool containsflipStr = true;
-    for (size_t i = 0; i < flipStr.size(); ++i)
-    {
-        if (newImagePrefix[newImagePrefix.size() - 1 - i] != flipStr[flipStr.size() - 1 - i]) {
+    for (size_t i = 0; i < flipStr.size(); ++i) {
+        if (newImagePrefix[newImagePrefix.size() - 1 - i] !=
+            flipStr[flipStr.size() - 1 - i]) {
             containsflipStr = false;
             break;
         }
     }
     if (containsflipStr) {
-        newImagePath = suffix == "" ? newImagePrefix.substr(0, newImagePrefix.size() - flipStr.size()) : newImagePrefix.substr(0, newImagePrefix.size() - flipStr.size()) + "." + suffix;
+        newImagePath = suffix == ""
+                           ? newImagePrefix.substr(
+                                 0, newImagePrefix.size() - flipStr.size())
+                           : newImagePrefix.substr(
+                                 0, newImagePrefix.size() - flipStr.size()) +
+                                 "." + suffix;
         if (isFileExists(newImagePath)) {
             std::cout << newImagePath << " already exists." << std::endl;
             system("pause");
@@ -62,15 +66,14 @@ int handle(char* ch) {
         system("pause");
         return 1;
     }
-    //std::cout << image.size << endl;
-    //std::cout << image.depth() << endl;
-    //std::cout << image.channels() << endl;
+    // std::cout << image.size << endl;
+    // std::cout << image.depth() << endl;
+    // std::cout << image.channels() << endl;
     if (image.channels() < 3) {
         std::cout << imagePath << " has less than 3 channels." << std::endl;
         system("pause");
         return 2;
-    }
-    else if (image.channels() == 3) {
+    } else if (image.channels() == 3) {
         switch (image.depth()) {
         case CV_16F:
         case CV_32F: {
@@ -90,13 +93,13 @@ int handle(char* ch) {
             break;
         }
         default: {
-            std::cout << imagePath << " has unknown image type." << std::endl;
+            std::cout << imagePath << " has unknown image type."
+                      << std::endl;
             system("pause");
             break;
         }
         }
-    }
-    else if (image.channels() == 4) {
+    } else if (image.channels() == 4) {
         switch (image.depth()) {
         case CV_16F:
         case CV_32F: {
@@ -116,13 +119,13 @@ int handle(char* ch) {
             break;
         }
         default: {
-            std::cout << imagePath << " has unknown image type." << std::endl;
+            std::cout << imagePath << " has unknown image type."
+                      << std::endl;
             system("pause");
             break;
         }
         }
-    }
-    else {
+    } else {
         std::cout << imagePath << " has morn than 4 channels." << std::endl;
         system("pause");
         return 3;
@@ -130,38 +133,28 @@ int handle(char* ch) {
     std::vector<int> param;
     switch (image.depth()) {
     case CV_16F: {
-        param = { IMWRITE_EXR_TYPE , IMWRITE_EXR_TYPE_HALF };
+        param = {IMWRITE_EXR_TYPE, IMWRITE_EXR_TYPE_HALF};
         break;
     }
     case CV_32F: {
-        param = { IMWRITE_EXR_TYPE , IMWRITE_EXR_TYPE_FLOAT };
+        param = {IMWRITE_EXR_TYPE, IMWRITE_EXR_TYPE_FLOAT};
         break;
     }
     case CV_8U: {
         if (suffix == "png") {
-            param = { IMWRITE_PNG_COMPRESSION , 1 };
-        }
-        else if (suffix == "jpg" || suffix == "jpeg") {
-            param = { IMWRITE_JPEG_QUALITY , 95 };
+            param = {IMWRITE_PNG_COMPRESSION, 1};
+        } else if (suffix == "jpg" || suffix == "jpeg") {
+            param = {IMWRITE_JPEG_QUALITY, 95};
         }
         break;
     }
     }
-    /*
-    int i = 1;
-    while (isFileExists(newImagePath)) {
-        newImagePath = newImagePrefix + flipStr + std::to_string(i) + "." + suffix;
-        ++i;
-    }
-    */
     imwrite(newImagePath, image, param);
     std::cout << "Complete." << std::endl;
     return 0;
 }
 
-
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
     if (argc <= 1) {
         std::cout << "Please input image files." << std::endl;
         return 0;
